f592d1287a78751b7c26bb0a72f22010f9e61cad
IsabelGuMo <gm.isabel@gmail.com>
2022-10-20 21:42:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="flex flex-col items-center">
+      <h1 class="text-border-4 text-9xl">Tlb</h1>
+      <h1 class="text-border-4 text-9xl">Soul</h1>
+      <h3 class="text-border-2 text-2xl">by IsabelGM</h3>
+    </div>
+    
+    <RouterView />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+<style>
+  
+  
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+
+<main>
+  
+  <div class="grid grid-cols-4 gap-20">
+    
+    <div v-if="loading" class="animate-spin h-20 w-20 mr-3" viewBox="0 0 24 24">
+      <img src="https://cdn.dribbble.com/users/1573707/screenshots/3712012/aug-4-2017-6-11-pm.gif">
+    </div>
+
+    <div v-for="jewel in data" :key="jewel.id" class="w-80 h-80 bg-slate-400/70 rounded grid text-center text-slate-400 text-xl justify-center">
+    {{jewel.title}}
+    <img class="w-64 h-64 bg-black" :src="`${jewel.url}`" alt="jewels">
+    </div>
+
+  </div> 
+ 
+</main>
+
+  
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      data: null,
+      loading: false  
+    };
+  },
+  methods: {
+    getJewels() {
+      this.loading = true;
+      axios.get('http://localhost:8080/jewels')
+      .then(response => {
+        this.data = response.data
+        
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .then (() => { 
+        this.loading = false;
+      })
+    },
+    getId(url) {
+      return url.split('/').reverse()[1];
+    },
+  },
+
+  mounted() {
+    this.getJewels();
+  }
+};
+</script>
